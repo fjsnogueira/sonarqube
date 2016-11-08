91133eeeeefd19f3b769df46512215c1e6b682a4
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2016-11-08 18:13:34 +0100

    SONAR-8332 make RequestUidGeneratorImpl support any number of HTTP request
    up to LONG.MAX_VALUE
    thanks to renewal of internal UuidGenerator.WithFixedBase instance every 4_194_304 requests

--- server/sonar-server/src/test/java/org/sonar/server/platform/web/requestid/RequestUidGeneratorImplTest.java
+++ server/sonar-server/src/test/java/org/sonar/server/platform/web/requestid/RequestUidGeneratorImplTest.java
@@ -19,15 +19,44 @@
  */
 package org.sonar.server.platform.web.requestid;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.sonar.core.util.UuidGenerator;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class RequestUidGeneratorImplTest {
-  @Test
-  public void generate_returns_value_if_RequestUidGeneratorBase_generate_encoded_in_base64() {
-    RequestUidGeneratorImpl underTest = new RequestUidGeneratorImpl(increment -> new byte[] {124, 22, 66, 96, 55, 88, 2, 9});
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
 
+  @Test
+  public void generate_renews_inner_UuidGenerator_instance_every_number_of_calls_to_generate_specified_in_RequestIdConfiguration() {
+    UuidGenerator.WithFixedBase generator1 = increment -> new byte[] {124, 22, 66, 96, 55, 88, 2, 9};
+    UuidGenerator.WithFixedBase generator2 = increment -> new byte[] {0, 5, 88, 81, 8, 6, 44, 19};
+    UuidGenerator.WithFixedBase generator3 = increment -> new byte[] {126, 9, 35, 76, 2, 1, 2};
+    RequestUidGeneratorBase uidGeneratorBase = mock(RequestUidGeneratorBase.class);
+    IllegalStateException expected = new IllegalStateException("Unexpected third call to createNew");
+    when(uidGeneratorBase.createNew())
+      .thenReturn(generator1)
+      .thenReturn(generator2)
+      .thenReturn(generator3)
+      .thenThrow(expected);
+
+    RequestUidGeneratorImpl underTest = new RequestUidGeneratorImpl(uidGeneratorBase, new RequestIdConfiguration(2));
+
+    assertThat(underTest.generate()).isEqualTo("fBZCYDdYAgk="); // using generator1
     assertThat(underTest.generate()).isEqualTo("fBZCYDdYAgk=");
+    assertThat(underTest.generate()).isEqualTo("AAVYUQgGLBM="); // call 2
+    assertThat(underTest.generate()).isEqualTo("AAVYUQgGLBM=");
+    assertThat(underTest.generate()).isEqualTo("CCC");
+    assertThat(underTest.generate()).isEqualTo("DDD");
+
+    expectedException.expect(IllegalStateException.class);
+    expectedException.expectMessage(expected.getMessage());
+
+    underTest.generate();
   }
 }
