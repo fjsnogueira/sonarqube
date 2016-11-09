b1bbeafefbcfceab5c08243222d4d9d515f7bc07
Julien Lancelot <julien.lancelot@sonarsource.com>
2016-11-09 19:16:12 +0100

    SONAR-8377 Remove issue filters dao

--- server/sonar-server/src/test/java/org/sonar/server/issue/filter/IssueFilterWsModuleTest.java
+++ server/sonar-server/src/test/java/org/sonar/server/issue/filter/IssueFilterWsModuleTest.java
@@ -29,6 +29,6 @@ public class IssueFilterWsModuleTest {
   public void verify_count_of_added_components() {
     ComponentContainer container = new ComponentContainer();
     new IssueFilterWsModule().configure(container);
-    assertThat(container.size()).isEqualTo(2);
+    assertThat(container.size()).isEqualTo(4);
   }
 }
